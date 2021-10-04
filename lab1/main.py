fe635e273698f1568971310161b2d76942d21093
misha <zenyatoop@gmail.com>
2021-10-04 19:55:24 +0300

    final version

--- lab1/main.py
+++ lab1/main.py
@@ -3,11 +3,18 @@ import matplotlib.pyplot as plt
 import itertools
 import types
 from typing import List
+from prettytable import PrettyTable
+
+import os
 
 # Consts
 PI = math.pi
 piLog2_5PI = PI * math.log2(5 * PI)
 
+hoard_table = PrettyTable()
+newton_table = PrettyTable()
+hoard_table.field_names = ["Iteration", "First interval", "Second interval", "X value"]
+newton_table.field_names = ["Iteration", "hk", "Xk-1", "Xk"]
 '''
     Zinchuk Mykhailo KV-94 Var 7 ->  22
     Hoard method
@@ -61,7 +68,7 @@ def draw_graph(x1, x2, f, title, f_name):
     plt.title(title)
     plt.plot(x_arr, y_arr)
     plt.grid(True)
-    plt.savefig(f"./results/{f_name}")
+    plt.savefig(f"./lab1/results/{f_name}")
     plt.show()
 
 
@@ -69,12 +76,24 @@ def draw_graph(x1, x2, f, title, f_name):
 draw_graph(-1, 4, f1.as_func, f1.as_str, "f1_graphic.png")
 draw_graph(-0.5, 1.3, f2.as_func, f2.as_str, "f2_graphic.png")
 
-# isolation interval found from graphic
-f1.boundaries.append([-2.0, 0])
-f1.boundaries.append([1.0, 1.5])
 
-f2.boundaries.append([0.5, 0.75])
-f2.boundaries.append([1, 1.25])
+def get_intervals(f, rec1, rec2):
+    print(f"\nEnter the  boundaries for {f.as_str}\n "
+          f"Recommended is {str(rec1)} {str(rec2)}\n")
+    a = input("[a, b], [a, b] -> enter with a space between -> for example(0 1 2 4) ")
+    bound = []
+    bound.extend(map(float, a.split(" ")))
+    return bound
+
+
+# isolation interval found from graphic
+bnd = get_intervals(f1, [-2.0, 0], [1.0, 1.5] )
+f1.boundaries.append(bnd[:2])
+f1.boundaries.append(bnd[2:])
+
+bnd = get_intervals(f2, [0.5, 0.75], [1.0, 1.25])
+f2.boundaries.append(bnd[:2])
+f2.boundaries.append(bnd[2:])
 
 
 def hoard_method(a: float, b: float, eps: float, f) -> List[float]:
@@ -89,11 +108,11 @@ def hoard_method(a: float, b: float, eps: float, f) -> List[float]:
     result = list()
 
     for i in itertools.count(0):
-        c = a - (f(a) * (b - a)) / (f(b) - f(a))  # (1.4)
+        c = a - (f(a) * (b - a)) / (f(b) - f(a))
         result.append(c)
-
+        hoard_table.add_row([i, a, b, c])
         if len(result) != 1:
-            if math.fabs(f(c)) < eps:
+            if math.fabs(c - result[-2]) < eps:
                 return result
 
         if f(a) * f(c) < 0:
@@ -120,6 +139,10 @@ def check(a: float, b: float, f, d2f) -> float:
         return b
 
 
+def get_k(i):
+    return 1 / (2 ** i)
+
+
 def newton_method(a: float, b: float, eps: float, f) -> List[float]:
     """
     Realization of Newton method
@@ -131,17 +154,20 @@ def newton_method(a: float, b: float, eps: float, f) -> List[float]:
     :return: List of results on every iteration
     """
     result = list()
-    h = 0.001
+    h = 0
     val = check(a, b, f2.as_func, f2.as_derivative2)
     result.append(val)
     xk_1 = None
+    i = 0
     while True:
         if xk_1 is not None:
             xk = xk_1
         else:
             xk = val
-
+        h = get_k(i)
+        i += 1
         xk_1 = xk - ((f(xk) * h) / (f(xk + h) - f(xk)))
+        newton_table.add_row([i, h, xk_1, xk])
         result.append(xk_1)
         if math.fabs(xk_1 - xk) < eps:
             break
@@ -182,32 +208,49 @@ def draw_results(result1: List[float], result2: List[float], title, f_nms: types
 
     plt.subplots_adjust(top=0.92, bottom=0.1, left=0.1, right=0.95, hspace=0.25,
                         wspace=0.35)
-    plt.savefig(f"./results/{f_name}")
+    plt.savefig(f"./lab1/results/{f_name}")
     plt.show()
 
 
-print("""
-                    Hoard method
-       x^2 + pi*log2(5*pi) - 7*pi*cos(x) - 3*x = 0\n"""
-      )
+def print_info(title, table, results):
+    print(f"___________________{title}__________________")
+    print(table)
+    table.clear_rows()
+    print("Result: " + str(results[-1]) + "\n")
 
-print(f"x1 ->  {hoard_method(f1.boundaries[0][0], f1.boundaries[0][1], 10 ** -7, f1.as_func)[-1]} - {f1.boundaries[0]}")
-print(f"x2 ->  {hoard_method(f1.boundaries[1][0], f1.boundaries[1][1], 10 ** -7, f1.as_func)[-1]} - {f1.boundaries[1]}")
 
-print("""\n
-                Discrete Newton method
-        e^x + sin(x) - 10x + x^10 + e^cos(x) = 0
-""")
+hoard_result_1_interval = hoard_method(f1.boundaries[0][0], f1.boundaries[0][1], 10 ** -7, f1.as_func)
+print_info("Hoard method", hoard_table, hoard_result_1_interval)
 
-print(
-    f"\n x1 -> {newton_method(f2.boundaries[0][0], f2.boundaries[0][1], 10 ** -7, f2.as_func, f2.as_derivative2)[-1]} - {f2.boundaries[0]}")
-print(
-    f" x2 -> {newton_method(f2.boundaries[1][0], f2.boundaries[1][1], 10 ** -7, f2.as_func, f2.as_derivative2)[-1]} - {f2.boundaries[1]}")
+hoard_result_2_interval = hoard_method(f1.boundaries[1][0], f1.boundaries[1][1], 10 ** -7, f1.as_func)
+print_info("Hoard method", hoard_table, hoard_result_2_interval)
 
-draw_results(hoard_method(f1.boundaries[0][0], f1.boundaries[0][1], 10 ** -7, f1.as_func),
-             hoard_method(f1.boundaries[1][0], f1.boundaries[1][1], 10 ** -7, f1.as_func),
+newton_result_1_interval = newton_method(f2.boundaries[0][0], f2.boundaries[0][1], 10 ** -7, f2.as_func)
+print_info("Discrete Newton method", newton_table, newton_result_1_interval)
+
+newton_result_2_interval = newton_method(f2.boundaries[1][0], f2.boundaries[1][1], 10 ** -7, f2.as_func)
+print_info("Discrete Newton method", newton_table, newton_result_2_interval)
+
+draw_results(hoard_result_1_interval, hoard_result_2_interval,
              "Hoard method \n" + f1.as_str, f1, "hoard_method.png")
 
-draw_results(newton_method(f2.boundaries[0][0], f2.boundaries[0][1], 10 ** -7, f2.as_func, f2.as_derivative2),
-             newton_method(f2.boundaries[1][0], f2.boundaries[1][1], 10 ** -7, f2.as_func, f2.as_derivative2),
-             "Newton method \n" + f2.as_str, f2, "newton_method.png")
+draw_results(newton_result_1_interval, newton_result_2_interval,
+             "Discrete Newton method \n" + f2.as_str, f2, "newton_method.png")
+
+# print("""
+#                     Hoard method
+#        x^2 + pi*log2(5*pi) - 7*pi*cos(x) - 3*x = 0\n"""
+#       )
+#
+# print(f"x1 ->  {hoard_method(f1.boundaries[0][0], f1.boundaries[0][1], 10 ** -7, f1.as_func)[-1]} - {f1.boundaries[0]}")
+# print(f"x2 ->  {hoard_method(f1.boundaries[1][0], f1.boundaries[1][1], 10 ** -7, f1.as_func)[-1]} - {f1.boundaries[1]}")
+#
+# print("""\n
+#                 Discrete Newton method
+#         e^x + sin(x) - 10x + x^10 + e^cos(x) = 0
+# """)
+#
+# print(
+#     f"\n x1 -> {newton_method(f2.boundaries[0][0], f2.boundaries[0][1], 10 ** -7, f2.as_func)[-1]} - {f2.boundaries[0]}")
+# print(
+#     f" x2 -> {newton_method(f2.boundaries[1][0], f2.boundaries[1][1], 10 ** -7, f2.as_func)[-1]} - {f2.boundaries[1]}")
